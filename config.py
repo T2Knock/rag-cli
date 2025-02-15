1eb57ff9637fd27116549a251c7f6bd4df9dbb6c
knock-huynh <huynhtransongngoc@gmail.com>
2025-02-15 16:52:07 +0700

    feat(env): load in env with dotenv

---
+++ config.py
@@ -0,0 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+langsmith_api_key = os.getenv("LANGSMITH_API_KEY")
+langsmith_api_key = os.getenv("LANGSMITH_API_KEY")
+
+google_ai_api_key = os.getenv("GOOGLE_AI_API_KEY")
