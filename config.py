00aa2e7df839e9b25ba99d12ddd7430e26d81f19
knock-huynh <huynhtransongngoc@gmail.com>
2025-02-20 23:32:23 +0700

    refactor(llm): remove langchain favour first party package

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
-from pydantic import Field, SecretStr
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
-    GOOGLE_AI_API_KEY: SecretStr
+    GOOGLE_AI_API_KEY: str
     LANGSMITH_TRACING: bool = Field(True)
     LANGSMITH_API_KEY: str
 
--- main.py
+++ main.py
@@ -1,21 +1,13 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
-
+import google.generativeai as genai
 import config
 
 
 def main():
-    llm = ChatGoogleGenerativeAI(
-        model="gemini-1.5-flash", api_key=config.setings.GOOGLE_AI_API_KEY
-    )
-    messages = [
-        (
-            "system",
-            "You are a helpful assistant that translates English to French. Translate the user sentence.",
-        ),
-        ("human", "I love programming."),
-    ]
-    ai_msg = llm.invoke(messages)
-    print(ai_msg)
+    genai.configure(api_key=config.setings.GOOGLE_AI_API_KEY)
+    model = genai.GenerativeModel(model_name="gemini-1.5-flash")
+    response = model.generate_content("Who is Ho Chi Minh")
+
+    print(response.text)
 
 
 if __name__ == "__main__":
